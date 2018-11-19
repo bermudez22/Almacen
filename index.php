a7ababd34d5ea67e3eabc757582db3f22c3d0ae0
Juan <Juan@DESKTOP-NSR69RC>
2018-11-19 22:48:50 +0100

    se conecta con el dni de bbdd. Esta a medias el dieseño. Los demas datos de la tabla usuarioalmacen hay que conectarlos

--- index.php
+++ index.php
@@ -17,11 +17,11 @@
        <?php
         $conexion = new mysqli('localhost', 'root', '', 'almacen');
         $consulta = $conexion -> query("Set names utf8");
-        $consulta = $conexion -> query("SELECT * FROM usuarios2 "); 
+        $consulta = $conexion -> query("SELECT * FROM usuariosalmacen"); 
         ?>
-        <div class="container-fluid">
-            <div class ="row-fluid">
-                <div class="span4">
+        <div class="container"style="background-color: blue">
+            <div class ="row-fluid" style="background-color: green">
+                <div class="span4" style="background-color: yellow">
                   <select name="listaNombres" id="lista">
                       <?php
                       while ($fila = $consulta->fetch_assoc()) {
@@ -29,12 +29,44 @@
                               echo '<option>'.$fila["DNI"].'</option>';  
                           }
                       }
+                      
                       ?>
                   </select>   
                 </div>
+                
                 <div class="span8" id='marcoCentral'>
                     
                 </div>
+                <div id="contenedor">
+        
+         <br/><br/>
+        <label>EMAIL</label>
+        <input type="text" id="numero">
+
+        <br/><br/>
+        <label>NOMBRE</label>
+        <input type="text" id="nombre"> 
+
+        <br/><br/>
+        <label>APELLIDO</label>
+        <input type="text" id="apellido"> 
+
+       
+        <br/><br/><br/>
+
+        <select class="test" id="tecnologia" name="tecnologia[]" multiple="multiple">
+            <option>2G</option> 
+            <option>3G</option> 
+            <option>4G</option> 
+            <option>TDD</option>
+        </select>
+
+        </br></br><br/><br/>
+        <button onclick="anadir();">Añadir</button>
+
+        <input type="submit" onclick="buscar()" value="buscar">
+
+    </div>
             </div>
             
         </div>
@@ -50,5 +82,6 @@
                 });
             });
         </script>
+        
     </body>
 </html>
